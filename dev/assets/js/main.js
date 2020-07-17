794044c5c4064cec8290eb6d41cf287699e22abc
jells <jells@jells.jells>
2020-07-17 19:10:34 +0300

    сверстал и адаптировал слайдер на промо-экране

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -6,4 +6,17 @@ window.addEventListener('DOMContentLoaded', () => {
   burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('burger-button_open');
   });
+
+  $('#promo-slider').slick({
+    prevArrow: '<svg class="slick-prev slick-arrow"><use xlink:href="./assets/images/svg/symbol/sprite.svg#arrow"></use></svg>',
+    nextArrow: '<svg class="slick-next slick-arrow"><use xlink:href="./assets/images/svg/symbol/sprite.svg#arrow"></use></svg>',
+
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        dots: true
+      }
+    }]
+  });
 });
