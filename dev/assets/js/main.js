99c68d965e3571dc47611487b2ba059ec6ac44a9
Vyacheslav <parselian.study@yandex.ru>
2020-08-14 12:54:05 +0300

    сверстал секцию "наши услуги" + доработал функцию пересборки слайдов

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -2,26 +2,32 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   const burgerBtn = document.querySelector('header .burger-button'),
-        studyCardsWrap = document.querySelector('#study-cards-slider'),
+        studyCardsSlider = document.getElementById('study-cards-slider'),
         studyCardAmounts = document.querySelectorAll('.study__card-amount'),
-        studyCards = studyCardsWrap.querySelectorAll('.study__card');
+        studyCards = studyCardsSlider.querySelectorAll('.study__card'),
+        servicesCardsSlider = document.getElementById('services-cards__slider'),
+        servicesCards = servicesCardsSlider.querySelectorAll('.services__card');
 
   burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('burger-button_open');
   });
 
-  $('#promo-slider').slick({
-    prevArrow: '<svg class="slider__arrow-prev slick-prev slick-arrow"><use xlink:href="./assets/images/svg/symbol/sprite.svg#arrow"></use></svg>',
-    nextArrow: '<svg class="slider__arrow-next slick-next slick-arrow"><use xlink:href="./assets/images/svg/symbol/sprite.svg#arrow"></use></svg>',
-
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        dots: true
-      }
-    }]
-  });
+  /* SLIDERS */
+  const activatePromoSlider = () => {
+    $('#promo-slider').slick({
+      prevArrow: '<svg class="slider__arrow-prev slick-prev slick-arrow"><use xlink:href="./assets/images/svg/symbol/sprite.svg#arrow"></use></svg>',
+      nextArrow: '<svg class="slider__arrow-next slick-next slick-arrow"><use xlink:href="./assets/images/svg/symbol/sprite.svg#arrow"></use></svg>',
+  
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          dots: true
+        }
+      }]
+    });
+  };
+  activatePromoSlider();
 
   const activateStudyCardsSlider = () => {
     $('#study-cards-slider').slick({
@@ -30,6 +36,15 @@ window.addEventListener('DOMContentLoaded', () => {
       slidesToShow: 1,
     });
   };
+
+  const activateServicesCardsSlider = () => {
+    $('#services-cards__slider').slick({
+      prevArrow: '<svg class="services-cards__slider-prev slick-prev slick-arrow"><use xlink:href="./assets/images/svg/symbol/sprite.svg#arrow"></use></svg>',
+      nextArrow: '<svg class="services-cards__slider-next slick-next slick-arrow"><use xlink:href="./assets/images/svg/symbol/sprite.svg#arrow"></use></svg>',
+    });
+  };
+  // activateServicesCardsSlider();
+
 
   const correctElemPosition = () => {
     studyCardAmounts.forEach(item => {
@@ -40,15 +55,15 @@ window.addEventListener('DOMContentLoaded', () => {
   };
   correctElemPosition();
 
-  const reassemblyStudyCardsSlides = () => {
+  const reassemblySlides = (sliderWrap, selector, sliderCards, xlAmount, lgAmount, mdAmount, smAmount, xsAmount, startSlider) => {
     let slideWrap = document.createElement('div'),
         counter = 0;
 
-    slideWrap.classList.add('study-cards__slide');
+    slideWrap.classList.add(selector);
 
     function removeSlides() {
-      studyCardsWrap.childNodes.forEach(item => {
-        if (item.classList && item.classList.contains('study-cards__slide')) {
+      sliderWrap.childNodes.forEach(item => {
+        if (item.classList && item.classList.contains(selector)) {
           item.remove();
         }
       });
@@ -58,35 +73,36 @@ window.addEventListener('DOMContentLoaded', () => {
     function assemblySlides() {
       let clonedSlideWrap = slideWrap.cloneNode(true);
 
-      studyCards.forEach((item, i) => {
+      const resetClonedSlide = (amount, i) => {
+        if (counter === amount || (counter <= amount && sliderCards.length - 1 === i)) {
+          sliderWrap.insertAdjacentElement('beforeend', clonedSlideWrap);
+          clonedSlideWrap = slideWrap.cloneNode(true);
+          counter = 0;
+        }
+      }
+
+      sliderCards.forEach((item, i) => {
         clonedSlideWrap.insertAdjacentElement('beforeend', item);
         counter++;
 
-        if (window.innerWidth > 768) {
-          if (counter === 6 || (counter < 7 && studyCards.length - 1 === i)) {
-            studyCardsWrap.insertAdjacentElement('beforeend', clonedSlideWrap);
-            clonedSlideWrap = slideWrap.cloneNode(true);
-            counter = 0;
-          }
+        if (window.innerWidth > 992) {
+          resetClonedSlide(xlAmount, i);
+        } else if (window.innerWidth <= 992 && window.innerWidth > 768) {
+          resetClonedSlide(lgAmount, i);
         } else if (window.innerWidth <= 768 && window.innerWidth > 576) {
-          if (counter === 4 || (counter < 5 && studyCards.length - 1 === i)) {
-            studyCardsWrap.insertAdjacentElement('beforeend', clonedSlideWrap);
-            clonedSlideWrap = slideWrap.cloneNode(true);
-            counter = 0;
-          }
-        } else if (window.innerWidth <= 576) {
-          if (counter === 2 || (counter < 3 && studyCards.length - 1 === i)) {
-            studyCardsWrap.insertAdjacentElement('beforeend', clonedSlideWrap);
-            clonedSlideWrap = slideWrap.cloneNode(true);
-            counter = 0;
-          }
+          resetClonedSlide(mdAmount, i);
+        } else if (window.innerWidth <= 576 && window.innerWidth > 460) {
+          resetClonedSlide(smAmount, i);
+        } else if (window.innerWidth <= 460) {
+          resetClonedSlide(xsAmount, i);
         }
       });
 
-      activateStudyCardsSlider();
+      startSlider();
     }
 
     removeSlides();
   };
-  reassemblyStudyCardsSlides();
+  reassemblySlides(studyCardsSlider, 'study-cards__slide', studyCards, 6, 6, 4, 2, 2, activateStudyCardsSlider);
+  reassemblySlides(servicesCardsSlider, 'services-cards__slide', servicesCards, 4, 3, 2, 2, 1, activateServicesCardsSlider);
 });
